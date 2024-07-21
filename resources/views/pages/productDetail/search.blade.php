e5a906c312e5109db5effa7f152bf80c4d0a1950
lkn2512 <lkn25122001@gmail.com>
2024-07-21 18:57:35 +0700

    tieuluan

--- resources/views/pages/productDetail/search.blade.php
+++ resources/views/pages/productDetail/search.blade.php
@@ -39,6 +39,35 @@
                             </div>
                         @endif
                     </div>
+                    <form>
+                        @csrf
+                        <input type="hidden" class="cart_product_id_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->product_id }}">
+                        <input type="hidden" class="cart_product_name_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->product_name }}">
+                        <input type="hidden" class="cart_product_image_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->product_image }}">
+                        <input type="hidden" class="cart_product_quantity_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->product_quantity }}">
+                        @if ($searh_pro->promotional_price > 0)
+                            <input type="hidden" class="cart_product_price_{{ $searh_pro->product_id }}"
+                                value="{{ $searh_pro->promotional_price }}">
+                        @else
+                            <input type="hidden" class="cart_product_price_{{ $searh_pro->product_id }}"
+                                value="{{ $searh_pro->product_price }}">
+                        @endif
+                        <input type="hidden" class="cart_category_product_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->category_name }}">
+                        <input type="hidden" class="cart_brand_product_{{ $searh_pro->product_id }}"
+                            value="{{ $searh_pro->brand_name }}">
+                        <input type="hidden" class="cart_product_qty_{{ $searh_pro->product_id }}" value="1">
+
+                        <div class="order-button">
+                            <a class="add-to-cart" data-id="{{ $searh_pro->product_id }}"><i
+                                    class="fa-solid fa-cart-arrow-down"></i>Đặt hàng
+                            </a>
+                        </div>
+                    </form>
                 </div>
             </div>
         @endforeach
